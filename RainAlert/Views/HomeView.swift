833b4a3d865a1415744c59cc9909aa5eda1dfd8d
elanghorne <ericlanghorne7@gmail.com>
2026-06-22 18:10:36 -0400

    tiny changes to planned ui

--- RainAlert/Views/HomeView.swift
+++ RainAlert/Views/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct HomeView: View {
     @State private var isPressed = false
     @State private var alertsOn = false
+    @StateObject private var settingsModel = SettingsModel()
 
     var body: some View {
         ZStack {
@@ -20,12 +21,13 @@ struct HomeView: View {
                 Spacer()
                 Spacer()
 
-                Text("RAIN ALERTS")
+                Text("RainAlert")
                     .font(.system(size: 25, weight: .semibold))
                     .tracking(1.2)
                     .foregroundColor(.black)
                     .padding(.bottom, 10)
-
+                Spacer()
+                Spacer()
                 Button(action: {
                     alertsOn = !alertsOn
                     print("Toggle button pressed")
@@ -74,6 +76,9 @@ struct HomeView: View {
                          )
                     }
                 }
+                alertsOn ? Text("Alerts: On") : Text("Alerts: Off")
+                Text("Morning Forecast: \(settingsModel.morningForecastTime)")
+                Spacer()
                 Spacer()
                 Spacer()
 
@@ -88,7 +93,7 @@ struct HomeView: View {
                     Button(action: {
                         print("Schedule button pressed")
                     }) {
-                        Text("Schedule")
+                        Text("Schedule Alerts")
                             .font(.system(size: 16, weight: .semibold))
                             .padding(.horizontal, 20)
                             .padding(.vertical, 10)
